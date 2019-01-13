1082ad5d347a396a12faf0d2c2e2c252a4900f93
Mai Bach <admin@Admins-MacBook-Air.local>
2019-01-13 22:08:24 +0700

    Adding test files

--- metro_rush.py
+++ metro_rush.py
@@ -4,25 +4,21 @@ from PathFinder import *
 
 
 def main():
-    metro = Metro('Delhi')
-    metro.build_graph('delhi')
-<<<<<<< HEAD
-    path, cost = PathFinding(metro).path
-    return (path, cost)
-        
+        try:
+                metro = Metro('Delhi')
+                metro.build_graph('delhi')
+                pathfinding = PathFinding(metro)
+                actionlist = pathfinding.get_action_list_1()
 
-if __name__ =='__main__':
-=======
-    pathfinding = PathFinding(metro)
-    actionlist = pathfinding.get_action_list_1()
+                for turn in actionlist:
+                        # inital state
+                        metro.update(turn)
+                        print('turn:', metro.turns)
+                        metro.print_train_location(-1)
+                        print()
+        except Exception:
+                pass
 
-    for turn in actionlist:
-        # inital state
-        metro.update(turn)
-        print('turn:', metro.turns)
-        metro.print_train_location(-1)
-        print()
 
 if __name__ == '__main__':
->>>>>>> master
     main()
